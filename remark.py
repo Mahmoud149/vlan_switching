5d27599f735f43f4e60294d276cbe152c7579a9e
oftutorial <oftutorial@openflowvm.home>
2014-11-16 21:57:47 -0200

    modified:   remark.py Meter added to the switch

--- remark.py
+++ remark.py
@@ -15,12 +15,10 @@
 
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
-from ryu.controller.handler import set_ev_cls
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, set_ev_cls
 from ryu.ofproto import ofproto_v1_3
-from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
-
+from ryu.lib.packet import packet,ethernet
+import time
 
 class SimpleSwitch13(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
@@ -31,41 +29,31 @@ class SimpleSwitch13(app_manager.RyuApp):
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
-        datapath = ev.msg.datapath
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-	#self.logger.debug("Debug:Someone is connecting")
-	#self.logger.info("Info:Someone is connecting")
+        DP = ev.msg.datapath
+        OF = DP.ofproto
+        parser = DP.ofproto_parser
+	self.logger.info("Someone is connecting")
         # install table-miss flow entry
         match = parser.OFPMatch()
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
-                                          ofproto.OFPCML_NO_BUFFER)]
-        self.add_flow(datapath, 0, match, actions)
+        actions = [parser.OFPActionOutput(OF.OFPP_CONTROLLER,
+                                          OF.OFPCML_NO_BUFFER)]
+        self.add_flow(DP, 0, match, actions)
+	self.add_Remark(DP,1,1000)
 
-    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+    def add_flow(self, datapath, priority, match, actions, meter_id=None):
         ofproto = datapath.ofproto
+
         parser = datapath.ofproto_parser
 
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-                                             actions)]
-        if buffer_id:
-            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
-                                    priority=priority, match=match,
+                               actions)]
+        if meter_id: inst.append(parser.OFPInstructionMeter(meter_id,ofproto.OFPIT_METER))
+        mod = parser.OFPFlowMod(datapath=datapath,priority=priority, match=match,
                                     instructions=inst)
-        else:
-            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                    match=match, instructions=inst)
         datapath.send_msg(mod)
-
-  
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
-        # If you hit this you might want to increase
-        # the "miss_send_length" of your switch
-        if ev.msg.msg_len < ev.msg.total_len:
-            self.logger.debug("packet truncated: only %s of %s bytes",
-                              ev.msg.msg_len, ev.msg.total_len)
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
@@ -81,8 +69,6 @@ class SimpleSwitch13(app_manager.RyuApp):
         dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
 
-        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
-
         # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
 
@@ -96,23 +82,29 @@ class SimpleSwitch13(app_manager.RyuApp):
         # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
             match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
-            # verify if we have a valid buffer_id, if yes avoid to send both
-            # flow_mod & packet_out
-            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-                return
-            else:
-                self.add_flow(datapath, 1, match, actions)
+                self.add_flow(datapath, 1, match, actions,1)
         data = None
-        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-            data = msg.data
 
         out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
                                   in_port=in_port, actions=actions, data=data)
         datapath.send_msg(out)
-    def add_meter(self,datapath,vlan):
-        now=int(round(time.time() * 1000))
-        delta=int(round(time.time() * 1000))-now
-        if delta>5 : self.logger.info("Meter from DPID %s took %s seconds to be processed", dpid, delta)
-    def getMeterId(self,datapath,vlan):
+
+    def add_Remark(self,dp,vlan,bw):
+        OF=dp.ofproto
+        parser=dp.ofproto_parser
+	burst_size=10
+        band=[]
+        band.append( parser.OFPMeterBandDscpRemark( rate=self.get_BW(vlan,10),
+                                              burst_size=burst_size,prec_level=7) )
+        meter_mod=parser.OFPMeterMod(datapath=dp,
+                                     command=OF.OFPMC_ADD,
+                                     flags=OF.OFPMF_KBPS,
+                                     meter_id=self.get_Meter_Id(dp,vlan),bands=band)
+	dp.send_msg(meter_mod)
+
+    def get_BW(self,vlan,bw):
+	return bw
+
+    def get_Meter_Id(self,datapath,vlan):
 	return vlan
+
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#../../bin/ryu-manager ../app/simple_switch.py
-../../bin/ryu-manager ../CS4270_Project/vlan_switching.py
+#ryu-manager ../app/simple_switch.py
+ryu-manager remark.py
